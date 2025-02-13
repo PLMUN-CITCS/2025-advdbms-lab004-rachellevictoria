9a58933d74cd6d4401e0895dbbe2f4d894bbf162
Rachelle <buenaobrarachellevictoria_bsit@plmun.edu.ph>
2025-02-13 19:28:36 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 02

--- university_db/alter_students_table.sql
+++ university_db/alter_students_table.sql
@@ -1,5 +1,2 @@
-Use `UniversityDB`;
-
--- Step 2: Alter the table to add an Email column
-ALTER TABLE `Students`
-ADD `Email` VARCHAR(100);
+Use `UniversityDB`; 
+-- Step 2: Alter the table to add an Email column ALTER TABLE `Students` ADD `Email` VARCHAR(100); 
